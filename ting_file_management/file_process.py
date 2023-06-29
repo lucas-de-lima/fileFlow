e6f375cbe9d79d50e361fd9e85971d752e7698aa
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-06-29 20:53:53 -0300

    feat: create the process function transform the contents of the list generated by the txt_importe

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.abstract_queue import AbstractQueue
+
+def process(path_file: str, instance: AbstractQueue):
+    # Verifica se o arquivo já foi processado anteriormente
+    for item in instance.queue:
+        if item["nome_do_arquivo"] == path_file:
+            # Arquivo já existente na fila, ignorar
+            return
+
+    # Realiza o processamento do arquivo
+    lines = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+    instance.enqueue(data)
+
+    # Exibe os dados processados via stdout
+    print(data)
 
 
 def remove(instance):
